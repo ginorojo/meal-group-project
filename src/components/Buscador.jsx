3a74121c7a451b58aec0fef2a9d4385a5d95a090
Kevin <kevinmanueldiazluis@gmail.com>
2025-05-30 00:51:24 -0700

    Cambios

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -8,7 +8,7 @@ export default function Buscador({ searchTerm, setSearchTerm }) {
 
   return (
     <form onSubmit={handleSearch} className="relative w-full max-w-md mx-auto mb-6 pt-5">
-      <div className="flex">
+      <div className="flex p-4 md:p-0">
         <input
           type="search"
           value={searchTerm}
@@ -16,7 +16,7 @@ export default function Buscador({ searchTerm, setSearchTerm }) {
           className="block w-full text-sm text-gray-900 bg-gray-50 rounded-l border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 p-2.5"
           placeholder="🔍Busca tu plato..."
         />
-        
+
       </div>
     </form>
   );
